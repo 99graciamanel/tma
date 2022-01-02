945762193d10b3dcc4e1e8aadae3db4fe3cecc78
musergi <sergi.mus@gmail.com>
2022-01-02 15:33:49 +0100

    Updated telegram bot

--- custom_webhook.py
+++ custom_webhook.py
@@ -1,4 +1,5 @@
 import logging
+import ipaddress
 import time
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -25,19 +26,6 @@ token = "5089246815:AAHAtnr5iV8twx-rIFgrS4PCoHWZnyF9alg"
 logging.basicConfig(format='%(threadName)s:%(name)s:%(levelname)s: %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def isValidIP(ip):
-    parts = ip.split(".")
-    if len(parts) != 4:
-        return False
-
-    for part in parts:
-        if not isinstance(int(part), int):
-            return False
-
-        if int(part) < 0 or int(part) > 255:
-            return False
-
-    return True
 
 class BannedIPs:
     def __init__(self, initial):
@@ -45,8 +33,7 @@ class BannedIPs:
         self.lock = threading.Lock()
 
     def add(self, ip):
-        if not isValidIP(ip):
-            raise ValueError(f'\"{ip}\" is not a valid ip')
+        ip = ipaddress.ip_address(ip)
         with self.lock:
             self.ips.add(ip)
 
@@ -57,6 +44,7 @@ class BannedIPs:
     def get_list(self):
         with self.lock:
             return list(self.ips)
+
 
 class BlacklistAlertWorker(threading.Thread):
     def __init__(self, banned_ips: BannedIPs, *args, **kwargs):
@@ -120,7 +108,7 @@ class BlacklistAlertWorker(threading.Thread):
 
 class DDoSAlertWorker(threading.Thread):
     def __init__(self, *args, **kwargs):
-        super().__init__(name='DDosTread', *args, **kwargs)
+        super().__init__(name='DDosThread', *args, **kwargs)
         self.url = "%s%s" % (url, '_count')
         self.content = {
           "query": {
@@ -169,6 +157,7 @@ class DDoSAlertWorker(threading.Thread):
         bot = telegram.Bot(token=token)
         bot.sendMessage(chat_id="-624999628", text=message)
 
+
 class AddressListWorker(threading.Thread):
     def __init__(self, banned_ips: BannedIPs, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -188,32 +177,36 @@ class AddressListWorker(threading.Thread):
 
     def get_help(self, update, context):
         update.message.reply_text(help_message)
+        logger.info('Showed help message')
 
     def get_blacklist(self, update, context):
         ip_list = self.banned_ips.get_list()
-        ip_str = map(lambda s: f'  - {s}', self.banned_ips)
+        ip_str = map(lambda s: f'  - {s}', ip_list)
         ip_str = '\n'.join(ip_str)
         update.message.reply_text(f'Banned IPs:\n{ip_str}')
+        logger.info('Showed IP list')
 
     def add_blacklist(self, update, context):
         try:
             ip = context.args[0]
             self.banned_ips.add(ip)
             update.message.reply_text(f'Added {ip} to banned IPs.')
+            logger.info(f'Added {ip} to banned IPs.')
         except(IndexError, ValueError):
             update.message.reply_text('/add_to_blacklist <Add a valid IP>')
 
     def remove_blacklist(self, update, context):
         try:
             ip = context.args[0]
-            banned_ips.remove(ip)
+            self.banned_ips.remove(ip)
             update.message.reply_text(f'Removed {ip} from banned IPs.')
+            logger.info(f'Removed {ip} from banned IPs.')
         except(IndexError, ValueError):
             update.message.reply_text('/remove_from_blacklist <Add a valid IP>')
 
     def error(self, update, context):
-        logger.error(f'Update \"{update}\" caused error \"{context.error}\"')
-        logger.error('Update services may be down')
+        logger.error(f'Update \"{update}\" caused error \"{context.error}\".')
+        logger.error('Update services may be down.')
 
 
 if __name__ == '__main__':
